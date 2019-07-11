25a7e41aa4130b7bcc6c4d4c7176aef87d3d1e13
Apple <apple@Apples-MacBook-Pro-87.local>
2019-07-11 16:28:50 -0500

    Iteration 0

--- ViewController.swift
+++ ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Emoji1: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
